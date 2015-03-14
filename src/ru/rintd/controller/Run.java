ae258083eeb58b6952771c9b8c1adfd0cfc2b0c8
sheihar <sheihar@B560>
2015-03-14 20:43:44 +0400

    Log4j2 add

--- src/ru/rintd/controller/Run.java
+++ src/ru/rintd/controller/Run.java
@@ -1,5 +1,8 @@
 package ru.rintd.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Загрузчик приложения, инициализирует контроллер и запускает главное окно
  * 
@@ -10,11 +13,13 @@ package ru.rintd.controller;
 public class Run {
 	
 	public static final String APPLICATION_NAME = "NodeMaker2";
+	private static final Logger log = LogManager.getLogger("Run");
 
 	private static Controller controller = null;
 
 	public static void main(String[] args) {
 
+		log.info("Application start...");
 		controller = new Controller();
 
 		controller.doShowMainWindow();
@@ -23,5 +28,6 @@ public class Run {
 		 * 
 		 * @Override public void run() { controller.doShowMainWindow(); } });
 		 */
+		log.info("Application stop");
 	}
 }
--- src/ru/rintd/view/AppWindow.java
+++ src/ru/rintd/view/AppWindow.java
@@ -2,18 +2,31 @@ package ru.rintd.view;
 
 import javax.swing.JFrame;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import ru.rintd.controller.Run;
 
 public class AppWindow {
 
 	private JFrame mainFrame;
 	private String applicationTitle = "";
+	private static final Logger log = LogManager.getLogger(AppWindow.class
+			.getName());
 
 	public AppWindow() {
 
+		log.info("Start intrface...");
 		mainFrame = new JFrame();
+		setDefaults();
 
+		mainFrame.setVisible(true);
+	}
+
+	public void setDefaults() {
+		log.info("Set default params in window");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setSize(1000, 800);
 		this.setTitle(null);
 	}
 
@@ -33,6 +46,7 @@ public class AppWindow {
 			applicationTitle = Run.APPLICATION_NAME;
 		}
 		mainFrame.setTitle(applicationTitle);
+		log.info("Set new title: " + applicationTitle + ", old: " + tmp);
 		return tmp;
 	}
 
@@ -40,10 +54,12 @@ public class AppWindow {
 	 * добавляет статусное сообщение в заголовок программы типа:
 	 * <b>"заголовок [ status ]"</b>
 	 * 
-	 * @param status статусное сообщение, вообще - люая строка
+	 * @param status
+	 *            статусное сообщение, вообще - люая строка
 	 */
 	public void setTitleStatus(String status) {
 		mainFrame.setTitle(applicationTitle + " [" + status + "]");
+		log.info("Set new status: " + status);
 	}
 
 }
